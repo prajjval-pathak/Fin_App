2341236e73b3a5479cdcf7b11be7d53a6aded795
Prajjval Pathak <prajjvalpathak@gmail.com>
2024-06-17 14:02:26 +0530

    Added Table Reusable Component

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>
+        This is he Design Guide for Our Finance App, This will content Reusable
+        Components
+      </h1>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -6,6 +6,7 @@ import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import DesignPage from "../Pages/DesignPage/DesignPage";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         element: <Hero />,
       },
       { path: "search", element: <SearchPage /> },
+      { path: "company-design", element: <DesignPage /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
